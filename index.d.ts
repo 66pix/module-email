b8bc398d41d55d62ba37fb62a8a33d07bfef35e1
Michael Robinson <mike@pagesofinterest.net>
2016-04-10 00:08:22 +1200

    Added definitions

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+/// <reference path="../typings/main/definitions/bluebird/index.d.ts" />
+/// <reference path="../typings-custom/email-templates.d.ts" />
+/// <reference path="../typings/nodemailer/nodemailer.d.ts" />
+import Promise = require('bluebird');
+import { IMailerSentMessageInfo } from './types';
+import { IMailerOptions } from './types';
+declare let mailer: (options: IMailerOptions) => Promise<IMailerSentMessageInfo>;
+export default mailer;
