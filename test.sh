7fa0fd5a995249eb0558f36824620e308a083ba5
Michael Robinson <mike@pagesofinterest.net>
2016-04-10 20:22:44 +1200

    Updated test scripot

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ set -o nounset
 set -o errexit
 
 mkdir -p coverage
-npm run build && ./node_modules/.bin/istanbul cover --include-all-sources -x gulpfile.js node_modules/.bin/_mocha -- --timeout 15000 --recursive --reporter spec test/
+npm run build && ./node_modules/.bin/istanbul cover --include-all-sources -x gulpfile.js node_modules/.bin/_mocha -- --timeout 15000 --recursive --reporter spec build/test/
 
 node_modules/.bin/istanbul report html
 node_modules/.bin/istanbul report text-summary > coverage/text-summary.txt
