83ea582edf3e2c03c2bcb8b9a82457a5c776b737
Michael Robinson <mike@pagesofinterest.net>
2016-05-10 20:54:18 +1200

    Updated typing

--- index.d.ts
+++ index.d.ts
@@ -1,28 +1,33 @@
 import Promise = require('bluebird');
 
 export interface IMailerOptionsContent {
-    subject: string;
-    baseUrl?: string;
-    html: string;
-    text: string;
-    [s: string]: any;
-}
-export interface IMailerOptions {
-    to: string;
-    subject: string;
-    text?: string;
-    html?: string;
-    from?: string;
-    content: IMailerOptionsContent;
-}
-export interface IMailerSentMessageInfo {
-    messageId: string;
-    envelope: any;
-    accepted: string[];
-    rejected: string[];
-    pending?: string[];
-    response: string;
+  subject: string;
+  baseUrl?: string;
+  html: string;
+  text: string;
+  [options: string]: any;
 }
 
-declare let mailer: (options: IMailerOptions) => Promise<IMailerSentMessageInfo>;
+export interface IMailerOptions {
+  to: string;
+  subject: string;
+  text?: string;
+  html?: string;
+  from?: string;
+  content: IMailerOptionsContent;
+}
+export interface IMailerSentMessageInfo {
+  messageId: string;
+  envelope: any;
+  accepted: string[];
+  rejected: string[];
+  pending?: string[];
+  response: string;
+}
+
+export interface IMailer {
+  (options: IMailerOptions): Promise<IMailerSentMessageInfo>;
+}
+
+declare let mailer: IMailer;
 export default mailer;
