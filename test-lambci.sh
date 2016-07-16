5ab137f4589c4d64b6dd670acc56a65825bcae24
Michael Robinson <mike@pagesofinterest.net>
2016-07-16 20:18:54 +1200

    lambci debug

--- test-lambci.sh
+++ test-lambci.sh
@@ -3,10 +3,19 @@
 set -o nounset
 set -o errexit
 
+echo "Building"
+
 npm install --silent
+
+echo "NSP"
+
 npm run nsp
+echo "Lint"
 npm run lint
+echo "typings install"
 npm run typings-install
+echo "build"
 npm run build
 
+echo "test"
 npm run test
