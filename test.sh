e22da5faddb57fd888f8c44faecb3cf0911f8969
Michael Robinson <mike@pagesofinterest.net>
2016-07-16 20:16:55 +1200

    Switch to lambci

--- test.sh
+++ test.sh
@@ -3,12 +3,20 @@
 set -o nounset
 set -o errexit
 
+export NODE_ENV="development"
+export EMAIL_HOST="localhost"
+export EMAIL_PASSWORD="email password"
+export EMAIL_USERNAME="email username"
+export EMAIL_PORT=1231
+export EMAIL_FROM="testing@66pix.com"
+export BASE_URL="https://66pix.com"
+
 mkdir -p coverage
 npm run build && ./node_modules/.bin/istanbul cover --include-all-sources -x gulpfile.js node_modules/.bin/_mocha -- --timeout 15000 --recursive --reporter spec build/test/
 
 node_modules/.bin/istanbul report html
 node_modules/.bin/istanbul report text-summary > coverage/text-summary.txt
 
-echo "\n"
+echo ""
 
 npm run coverage-average
