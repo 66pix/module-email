ec8475c2131e6f84228e944f507a81fba09d10fa
Michael Robinson <mike@pagesofinterest.net>
2016-07-16 20:29:54 +1200

    Removed lambci debug

--- test-lambci.sh
+++ test-lambci.sh
@@ -3,21 +3,11 @@
 set -o nounset
 set -o errexit
 
-echo "Building"
-
-ls -lh
-
-npm install
-
-echo "NSP"
+npm install --silent
 
 npm run nsp
-echo "Lint"
 npm run lint
-echo "typings install"
 npm run typings-install
-echo "build"
 npm run build
 
-echo "test"
 npm run test
