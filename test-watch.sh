7f5e5867967d6d7d78fa15cbf452f4664568a870
Michael Robinson <mike@pagesofinterest.net>
2016-07-23 10:54:37 +1200

    Updated tests to use istanbul remap

--- test-watch.sh
+++ test-watch.sh
@@ -37,14 +37,31 @@ export EMAIL_USERNAME="email username"
 export EMAIL_PORT=1231
 export EMAIL_FROM="support@66pix.com"
 
-mkdir -p coverage
+COVERAGE_DIR=coverage/raw
+REMAP_DIR=coverage/typescript
+
+mkdir -p $COVERAGE_DIR
+mkdir -p $REMAP_DIR
+
+echo "Running tests"
 nodemon -e ts \
-  -x 'npm run build && ./node_modules/.bin/istanbul cover --include-all-sources -x gulpfile.js node_modules/.bin/_mocha -- --timeout 15000 --recursive --reporter spec build/test/'
+  -x "npm run build && node_modules/.bin/istanbul cover --dir $COVERAGE_DIR node_modules/.bin/_mocha -- --timeout 15000 --recursive --reporter spec build/test/"
 
-node_modules/.bin/istanbul report html
-node_modules/.bin/istanbul report text-summary > coverage/text-summary.txt
+echo ""
+echo "Remapping coverage reports for typescript"
+node_modules/.bin/remap-istanbul -i $COVERAGE_DIR/coverage.json -o $REMAP_DIR -t html
+node_modules/.bin/remap-istanbul -i $COVERAGE_DIR/coverage.json -o $REMAP_DIR/coverage.json -t json
 
-echo "\n"
+echo ""
+echo "Coverage report located at $REMAP_DIR/index.html"
 
-npm run coverage-average
-
+COVERAGE_AVERAGE=90
+echo ""
+echo "Enforcing coverage average of $COVERAGE_AVERAGE for $REMAP_DIR/coverage.json"
+echo ""
+node_modules/.bin/istanbul check-coverage \
+  --statements $COVERAGE_AVERAGE \
+  --functions $COVERAGE_AVERAGE \
+  --branches $COVERAGE_AVERAGE \
+  --lines $COVERAGE_AVERAGE \
+  $REMAP_DIR/coverage.json
--- test.sh
+++ test.sh
@@ -11,12 +11,30 @@ export EMAIL_PORT=1231
 export EMAIL_FROM="testing@66pix.com"
 export BASE_URL="https://66pix.com"
 
-mkdir -p coverage
-npm run build && ./node_modules/.bin/istanbul cover --include-all-sources -x gulpfile.js node_modules/.bin/_mocha -- --timeout 15000 --recursive --reporter spec build/test/
+COVERAGE_DIR=coverage/raw
+REMAP_DIR=coverage/typescript
 
-node_modules/.bin/istanbul report html
-node_modules/.bin/istanbul report text-summary > coverage/text-summary.txt
+mkdir -p $COVERAGE_DIR
+mkdir -p $REMAP_DIR
+
+echo "Running tests"
+npm run build && node_modules/.bin/istanbul cover --dir $COVERAGE_DIR node_modules/.bin/_mocha -- --timeout 15000 --recursive --reporter spec build/test/
 
 echo ""
+echo "Remapping coverage reports for typescript"
+node_modules/.bin/remap-istanbul -i $COVERAGE_DIR/coverage.json -o $REMAP_DIR -t html
+node_modules/.bin/remap-istanbul -i $COVERAGE_DIR/coverage.json -o $REMAP_DIR/coverage.json -t json
 
-npm run coverage-average
+echo ""
+echo "Coverage report located at $REMAP_DIR/index.html"
+
+COVERAGE_AVERAGE=90
+echo ""
+echo "Enforcing coverage average of $COVERAGE_AVERAGE for $REMAP_DIR/coverage.json"
+echo ""
+node_modules/.bin/istanbul check-coverage \
+  --statements $COVERAGE_AVERAGE \
+  --functions $COVERAGE_AVERAGE \
+  --branches $COVERAGE_AVERAGE \
+  --lines $COVERAGE_AVERAGE \
+  $REMAP_DIR/coverage.json
